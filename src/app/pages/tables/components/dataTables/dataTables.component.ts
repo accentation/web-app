77a3b3f4f2b61c2bc2e1b65ed92515a8d8ed3890
DaNySkyrim <ibor.ps3@gmail.com>
2017-05-12 12:24:57 +0200

    Adding angluar2 skeleton

---
+++ src/app/pages/tables/components/dataTables/dataTables.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { DataTablesService } from './dataTables.service';
+
+import { Login } from "./login";
+
+@Component({
+  selector: 'data-tables',
+  templateUrl: './dataTables.html',
+  styleUrls: ['./dataTables.scss']
+})
+export class DataTables {
+
+    data;
+    filterQuery = "";
+    rowsOnPage = 10;
+    sortBy = "email";
+    sortOrder = "asc";
+    /*model = new Login('1', 'usuario', 'dad@faf.com', '1234');
+    user : Login;*/
+    constructor(private service: DataTablesService) {
+        
+    /*this.service.getData().then((data) => {
+      this.data = data;
+    });*/
+
+
+  }
+
+    toInt(num: string) {
+        return +num;
+    }
+
+    sortByWordLength = (a: any) => {
+        return a.city.length;
+    }
+    getUserClick() {
+      this.service.getUserFromService().subscribe(
+        datosRecibidos => {
+          this.data = datosRecibidos;
+          //this.route.navigate(['house-list']);
+          //console.log(data)
+          alert("GET RICIBIDO MUY BIEN");
+        }
+      );
+  }
+
+  
+}
+
